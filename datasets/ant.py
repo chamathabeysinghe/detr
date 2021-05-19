65c046710867dd72417a4dff8144289f517c4732
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-05-19 16:00:08 +1000

    Visualization script

--- datasets/ant.py
+++ datasets/ant.py
@@ -149,8 +149,8 @@ def build(image_set, args):
     assert root.exists(), f'provided Ant path {root} does not exist'
     mode = 'instances'
     PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+        "train": (root / "train", root / 'ground-truth-train.json'),
+        "val": (root / "test", root / 'ground-truth-test.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
