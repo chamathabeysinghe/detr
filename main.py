a976f04ba7ee0d23a7742160268e1ca36d506cda
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-05-25 18:39:51 +0530

    Freezing

--- main.py
+++ main.py
@@ -121,6 +121,12 @@ def main(args):
     model, criterion, postprocessors = build_model(args)
     model.to(device)
 
+    for n,p in model.named_parameters():
+        if "backbone" in n:
+            p.requires_grad_(False)
+        if "encoder" in n:
+            p.requires_grad_(False)
+
     model_without_ddp = model
     if args.distributed:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
@@ -175,8 +181,8 @@ def main(args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
-        # del checkpoint['model']['class_embed.weight']
-        # del checkpoint['model']['class_embed.bias']
+        del checkpoint['model']['class_embed.weight']
+        del checkpoint['model']['class_embed.bias']
         # del checkpoint['model']['query_embed.weight']
         model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
