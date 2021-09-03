61778b414e1a7b192566b6a085569702947e9cc0
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-09-03 11:53:41 +0530

    Revert "Temporary optimize GPU"
    This reverts commit 3542b776

--- engine.py
+++ engine.py
@@ -27,22 +27,13 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
 
     for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
         samples = samples.to(device)
-        outputs, encoder_out = model(samples)
-
-        del samples
-        outputs = [{k: v.to('cpu') for k, v in t.items()} for t in outputs]
-        encoder_out = encoder_out.to('cpu')
-
-        with torch.no_grad():
-            samples_val, _ = next(iter(data_loader_val))  # TODO pass only the iter
-            samples_val = samples_val.to(device)
-            _, encoder_out_val = model(samples_val)
-            del samples_val
-
-        outputs = [{k: v.to(device) for k, v in t.items()} for t in outputs]
-        encoder_out = encoder_out.to(device)
+        samples_val, targets_val = next(iter(data_loader_val))  # TODO pass only the iter
+        samples_val = samples_val.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
+        outputs, encoder_out = model(samples)
+        with torch.no_grad():
+            _, encoder_out_val = model(samples_val)
         loss_dict = criterion(outputs, targets, encoder_out, encoder_out_val)
         weight_dict = criterion.weight_dict
         losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
